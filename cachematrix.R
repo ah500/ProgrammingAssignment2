004de8382435bdaf80210b93a5dee030bd665ce4
Anna Hall <anna.hall@yahoo.co.uk>
2014-04-21 11:11:11 +0100

    Fill in makeCacheMatrix, including comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache the inverse value of a matrix to avoid repeated calculating it (as it's an expensive operation)
 
-## Write a short comment describing this function
+## A wrapper around matrix which adds get, set, setinverse and getinverse methods
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ## inverse will store the inverse, it is set to NULL until it has been calculated
+  inverse <- NULL
+  
+  ## Define the methods which can be called on cacheMatrix
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  
+  ## Return a list of the methods which can be called on cacheMatrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
